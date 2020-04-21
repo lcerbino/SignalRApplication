d63e7074d6c87d1b16a49b9379f12b023488db3b
Lucas Cerbino <Lucas.Cerbino@ey.com>
2020-04-21 15:26:05 -0300

    Add project files.

---
+++ SignalRApplication/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRApplication.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        private List<string> userConnections = new List<string>();
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
